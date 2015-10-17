9af00486175379b64451afb97180a09c71cede66
Shohei Kikuchi <shoppe_shopetan@yahoo.co.jp>
2015-10-17 08:59:10 +0000

    hello world

--- app/controllers/getorder.go
+++ app/controllers/getorder.go
@@ -6,6 +6,12 @@ type Getorder struct {
   *revel.Controller
 }
 
+func (c Getorder) Index() revel.Result {
+  greeting := "Hello World!"
+  message := "これはStep2の実装です。(その1)"
+  return c.Render(greeting, message)
+}
+
 func (c Getorder) Greet(greeting string) revel.Result {
   message := "これはStep2の実装です。(その2)"
   c.RenderArgs["greeting"] = greeting
